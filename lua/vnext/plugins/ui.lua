0b0ec21f5affaa9e49764ec2e0345c538ca532ad
Johan <22452724+johsve-source@users.noreply.github.com>
2025-09-02 06:54:00 +0200

    ci: changed to final colorscheme

--- lua/vnext/plugins/ui.lua
+++ lua/vnext/plugins/ui.lua
@@ -83,28 +83,12 @@ return {
   },
 
   {
-    "tiagovla/tokyodark.nvim",
-    opts = {
-      transparent_background = false, -- set background to transparent
-      gamma = 1.00, -- adjust the brightness of the theme
-      styles = {
-        comments = { italic = true }, -- style for comments
-        keywords = { italic = true }, -- style for keywords
-        identifiers = { italic = true }, -- style for identifiers
-        functions = {}, -- style for functions
-        variables = {}, -- style for variables
-      },
-      custom_highlights = function(highlights, palette)
-        return {}
-      end, -- extend highlights
-      custom_palette = function(palette)
-        return {}
-      end, -- extend palette
-      terminal_colors = true, -- enable terminal colors
-    },
-    config = function(_, opts)
-      require("tokyodark").setup(opts)
-      vim.cmd.colorscheme("tokyodark")
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme moonfly]])
     end,
   },
 }
